9836af40863a17f0976c8fa6a5022e1ab05801a4
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-25 01:13:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,23 +3,46 @@
 #include "alg.h"
 #include <iostream>
 
-int checkPrime(uint64_t value) {
-    int cou = 0;
-    int i = 2;
-    while (cou != value) {
-        boolean f = false;
-	for (int z = 1; z != i+1; z++) {
-	    if (z != 1 && z != i) {
-		if (i % z == 0) {
-		    f = true;
-		    break;
+bool checkPrime(uint64_t value) {
+    bool f = true;
+    int i = value;
+	    for (int z = 1; z < i; z++) {
+		    if (z != i && z != 1) {
+				if (i % z == 0) {
+				   f = false;
+				}
+			}
 		}
-	    }
-	}
-	if (f == false) {
-	    cou++;
-	}
-	    i++;
-    }
-    return i-1;
+		if (f == true) {
+		    return f;
+		}
+}
+
+uint64_t nPrime(uint64_t n) {
+    int i = n;
+		if (checkPrime(i) == 1) {
+		    return i;
+		}
+}
+
+uint64_t nextPrime(uint64_t value) {
+    int i = value;
+    bool f = false;
+		while (f != true) {
+	        i++;
+				if (checkPrime(i) == true) {
+			        return i;
+				}
+		}
+}
+
+uint64_t sumPrime(uint64_t hbound) {
+    int i = hbound;
+    int sum = 0;
+		for (int z = 2; z != i; z++) {
+			if (checkPrime(z) == true) {
+			    sum += z;
+			}
+		}
+	    return sum;
 }
