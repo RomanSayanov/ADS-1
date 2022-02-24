993c179686a4c23fd90d1f391610047c3cd1231f
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-25 02:34:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 // Copyright 2022 NNTU-CS
 #include "alg.h"
-using namespace std;
+#include <ostream>
 
 int main() {
-    int res1 = checkPrime(17);
-    int res2 = nPrime(3);
-    int res3 = nextPrime(18);
-    int res4 = sumPrime(18);
-    cout << res1 << "-check " << res2 << "-n " << res3 << "-next " << res4 << "-sum " << endl;
     return 0;
 }
