f6d9f7ec44b7b989a199a0c0f9af443aff58cef2
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-24 13:18:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,18 +8,18 @@ int checkPrime(uint64_t value) {
     int i = 2;
     while (cou != value) {
         boolean f = false;
-	for (int z = 1; z != i+1; z++) {
-		if (z != 1 && z != i) {
-			if (i % z == 0) {
-			    f = true;
-			    break;
-			}
-		}
-	}
-	if (f == false) {
-	    cou++;
-	}
-	    i++;
+	    for (int z = 1; z != i+1; z++) {
+		    if (z != 1 && z != i) {
+			    if (i % z == 0) {
+			        f = true;
+			        break;
+			    }
+		    }
+	    }
+	    if (f == false) {
+	        cou++;
+	    }
+	        i++;
     }
     return i-1;
 }
