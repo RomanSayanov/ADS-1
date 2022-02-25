cea66e3cd2001e081414fa6c5e823ea1c31c42aa
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-25 11:03:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include "alg.h"
 
 int main() {
-	int h = checkPrime(3);
-	int h1 = nPrime(2);
-	int h2 = nextPrime(10);
-	int h3 = sumPrime(14);
-	std::cout << h << std::endl;
-	std::cout << h1 << std::endl;
-	std::cout << h2 << std::endl;
-	std::cout << h3 << std::endl;
+    int h = checkPrime(3);
+    int h1 = nPrime(2);
+    int h2 = nextPrime(10);
+    int h3 = sumPrime(14);
+    std::cout << h << std::endl;
+    std::cout << h1 << std::endl;
+    std::cout << h2 << std::endl;
+    std::cout << h3 << std::endl;
 }
 
