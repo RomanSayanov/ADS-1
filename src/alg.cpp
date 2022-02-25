7907140ab0199c69892e63571b615f4adf1ff49d
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-25 09:59:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,53 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <iostream>
 
 bool checkPrime(uint64_t value) {
     bool f = true;
     int i = value;
-	for (int z = 1; z < i; z++) {
-		if (z != i && z != 1) {
-			if (i % z == 0) {
-			    f = false;
-			}
-		}
+    for (int z = 1; z < i; z++) {
+	if (z != i && z != 1) {
+	    if (i % z == 0) {
+		f = false;
+	    }
 	}
-	return f;
+    }
+    return f;
 }
 
 uint64_t nPrime(uint64_t n) {
     int i = n;
     int f = 1;
     int cou = 0;
-	while (cou != i) {
-	    f++;
-		if (checkPrime(f) == 1) {
-		    cou++;
-		}
+    while (cou != i) {
+	f++;
+	if (checkPrime(f) == 1) {
+	    cou++;
 	}
+    }
     return f;
 }
 
 uint64_t nextPrime(uint64_t value) {
     int i = value;
     bool f = false;
-	while (f != true) {
-	    i++;
-		if (checkPrime(i) == true) {
-		    return i;
-		    break;
-		}
+    while (f != true) {
+	i++;
+	if (checkPrime(i) == true) {
+		return i;
+		break;
 	}
+    }
     return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
     int i = hbound;
     int sum = 0;
-	for (int z = 2; z != i; z++) {
-		if (checkPrime(z) == true) {
-		    sum += z;
-		}
+    for (int z = 2; z != i; z++) {
+	if (checkPrime(z) == true) {
+	    sum += z;
 	}
+    }
     return sum;
 }
